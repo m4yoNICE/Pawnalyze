b30d5d9aee751516023e191809e9fb98515f70ee
m4yoNICE <cenizaroswellrey@gmail.com>
2026-05-05 12:53:52 +0800

    [feat][Roswell] changed llm prompt requirements to lessen token, initialized next js files and folder structure, installed chess package on next

--- backend/fastapi/chesslib/parser.py
+++ backend/fastapi/chesslib/parser.py
@@ -7,13 +7,17 @@ def parse_pgn(pgn_string):
     game = chess.pgn.read_game(pgn)
     return iterate_moves(game)
 
-
 def iterate_moves(game):
     board = game.board()
-    fens = []
+    positions = []
     for move in game.mainline_moves():
+        board_before = board.copy()
         board.push(move)
-        fens.append(board.fen())
-    return fens
+        positions.append({
+            "fen": board.fen(),
+            "move": move,
+            "board_before": board_before
+        })
+    return positions
 
 
